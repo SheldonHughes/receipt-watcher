05d8b5efdc2596061f6ddf82314d373319a47730
Sheldon <sheldondhughes@gmail.com>
2026-01-27 17:05:53 -0600

    Fixed some file paths

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import path from "path";
 export const INBOX_DIR = path.resolve("D:\\OneDrive\\Scans");
 
 export const RECEIPTS_ROOT = path.resolve(
-  "D:\OneDrive\\Hughes Financial's\\Receipts",
+  "D:\\OneDrive\\Hughes Financial's\\Receipts",
 );
 
 export const MANUAL_REVIEW_DIR = path.join(RECEIPTS_ROOT, "Manual Review");
