270f2ac20825d425aeaff0d58b63d39ee1fce458
SheldonHughes <sheldondhughes@gmail.com>
2026-01-29 22:41:57 -0600

    Begin integration of json file for settings and vendors

--- vendorDetector.js
+++ vendorDetector.js
@@ -1,18 +1,23 @@
 /**
  * Simple keyword-based vendor detection
  */
-const VENDOR_MAP = {
-  Posigen: ["posigen", "solar solutions"],
-  "Stop & Shop": ["stop n shop", "stop & shop", "stop&shop"],
-  Entergy: ["entergy", "electric service"],
-  Amazon: ["amazon.com", "amzn mktp"],
-  Shell: ["shell oil", "v-power"],
-  "Brotherhood Auto Aid": ["brotherhood auto aid", "baa"],
-  "J R Parkerson": ["j r parkerson iii", "parkerson"],
-  "Leland Auto Parts": ["leland auto parts", "bumper to bumper", "leland auto"],
-  "Double Quick": ["double quick", "doublequick"],
-  "Zoe Coffee Co.": ["Zoe Coffee company", "zoe coffee", "z0e coffee"],
-};
+import vendors from "./settings.json" with { type: "json" };
+
+const VENDOR_MAP = vendors.vendor;
+
+// console.log(VENDOR_MAP);
+// {
+//   Posigen: ["posigen", "solar solutions"],
+//   "Stop & Shop": ["stop n shop", "stop & shop", "stop&shop"],
+//   Entergy: ["entergy", "electric service"],
+//   Amazon: ["amazon.com", "amzn mktp"],
+//   Shell: ["shell oil", "v-power"],
+//   "Brotherhood Auto Aid": ["brotherhood auto aid", "baa"],
+//   "J R Parkerson": ["j r parkerson iii", "parkerson"],
+//   "Leland Auto Parts": ["leland auto parts", "bumper to bumper", "leland auto"],
+//   "Double Quick": ["double quick", "doublequick"],
+//   "Zoe Coffee Co.": ["Zoe Coffee company", "zoe coffee", "z0e coffee"],
+// };
 
 export function detectVendor(text) {
   const normalizedText = text.toLowerCase();
